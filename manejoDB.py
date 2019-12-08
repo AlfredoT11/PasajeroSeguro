8930f8ac615b250c59b9f8bc0154e884ec123580
Alfredo Diaz <alfredo117tdg@hotmail.com>
2019-12-07 20:32:02 -0600

    Conexión Azure mejorada

--- manejoDB.py
+++ manejoDB.py
@@ -2,9 +2,9 @@ import pymongo
 
 def nuevoRegistro(infoUsuario):
 
-    #uri = "mongodb://adiazg1404:Jx0UTPrHqPbDN8FwfrmCheZpOifEsRH0DzyYW5M4PilGkGzP8uQ8qT9xfSQQA9mjhwGIxhfF0ai0VEYAkvzmkQ==@adiazg1404.documents.azure.com:10255/?ssl=true&replicaSet=globaldb"
-    #client = pymongo.MongoClient(uri)
-    client = pymongo.MongoClient("mongodb://localhost:27017/")
+    uri = "mongodb://adiazg1404:Jx0UTPrHqPbDN8FwfrmCheZpOifEsRH0DzyYW5M4PilGkGzP8uQ8qT9xfSQQA9mjhwGIxhfF0ai0VEYAkvzmkQ==@adiazg1404.documents.azure.com:10255/?ssl=true&replicaSet=globaldb"
+    client = pymongo.MongoClient(uri)
+    #client = pymongo.MongoClient("mongodb://localhost:27017/")
     db = client["TransporteSeguro"]
     collectionUsuario = db["Usuario"]
 
@@ -15,10 +15,10 @@ def nuevoRegistro(infoUsuario):
 
 def buscarUsuario(idUsuario):
 
-    #uri = "mongodb://adiazg1404:Jx0UTPrHqPbDN8FwfrmCheZpOifEsRH0DzyYW5M4PilGkGzP8uQ8qT9xfSQQA9mjhwGIxhfF0ai0VEYAkvzmkQ==@adiazg1404.documents.azure.com:10255/?ssl=true&replicaSet=globaldb"
-    #client = pymongo.MongoClient(uri)
+    uri = "mongodb://adiazg1404:Jx0UTPrHqPbDN8FwfrmCheZpOifEsRH0DzyYW5M4PilGkGzP8uQ8qT9xfSQQA9mjhwGIxhfF0ai0VEYAkvzmkQ==@adiazg1404.documents.azure.com:10255/?ssl=true&replicaSet=globaldb"
+    client = pymongo.MongoClient(uri)
 
-    client = pymongo.MongoClient("mongodb://localhost:27017/")
+    #client = pymongo.MongoClient("mongodb://localhost:27017/")
     db = client["TransporteSeguro"]
     collectionUsuario = db["Usuario"]
 
--- validacionArduino.py
+++ validacionArduino.py
@@ -1,5 +1,5 @@
 import serial, time
-from manejoDB import buscarUsuario
+from manejoDB import buscarUsuario, nuevoRegistro
 
 def verificarDatosEnviados():
     
@@ -7,12 +7,25 @@ def verificarDatosEnviados():
     print("Iniciando...")
     time.sleep(3)
     print("Inicio exitoso")
+ 
 
-    for i in range(2):
+    for i in range(10):
         identificador = infoPuerto.readline()
-        print("Identificador recibido: ", identificador)
-        print("Tipo: ", type(identificador))
-        print("String: ", identificador.decode("utf-8"))
+        #print("Identificador recibido: ", identificador)
+        #print("Tipo: ", type(identificador))
+        #print("String: ", identificador.decode("utf-8"))
+
+        #usuarioNuevo = {
+        #    "_id" : identificador.decode("utf-8")[0:len(identificador.decode("utf-8"))-2],
+        #    "nombre" : "Alfredo",
+        #    "segundoNombre" : "Tonatiuh",
+        #    "paterno" : "Díaz",
+        #    "materno" : "Gómez",
+        #    "escuela" : "ESCOM"
+        #}
+
+        #nuevoRegistro(usuarioNuevo)
+
         buscarUsuario(identificador.decode("utf-8")[0:len(identificador.decode("utf-8"))-2])
 
 
